e9594d6da628ab7812f7cbf2171da3f0735a89a9
hieuche123 <86963416+hieuche123@users.noreply.github.com>
2021-07-07 13:06:39 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,31 @@ const $$ = document.querySelectorAll.bind(document);
 const huhu = $('html')
 const li = $$('li')
 const hihi = $('.music')
+
 huhu.onclick = function () {
     hihi.play();
 }
 li.onclick = function () {
     hihi.play();
 }
+
+huhu.onwheel = function () {
+    hihi.play();
+}
+li.onwheel = function () {
+    hihi.play();
+}
+
+huhu.onmouseup = function () {
+    hihi.play();
+}
+li.onmouseup = function () {
+    hihi.play();
+}
+huhu.onmouseover = function () {
+    hihi.play();
+}
+li.onmouseover = function () {
+    hihi.play();
+}
+
