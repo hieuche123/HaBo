d63ccc74a020655e31a2930892c49c45acfbf5c3
hieuche123 <86963416+hieuche123@users.noreply.github.com>
2021-07-09 01:20:23 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,43 +2,68 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 const huhu = $('html')
 const control = $('.control')
+const continues = $('.continue')
 const li = $$('li')
+const message1 = $('.message1')
+const message2 = $('.message2')
+const clockdiv = $('.clockdiv')
+const smalltext = $$('.smalltext')
 const hihi = $('.music')
+const happyBirthday = $('.happy-birthday')
+const videos = $('.videos')
+console.log(message1)
 
-huhu.onclick = function () {
-    hihi.play();
-}
-li.onclick = function () {
-    hihi.play();
-}
-huhu.onscroll = function () {
-    hihi.play();
-}
-document.onscroll = function () {
-    hihi.play();
-}
-$$.onscroll = function () {
-    hihi.play();
-}
-$.onscroll = function () {
-    hihi.play();
-}
-huhu.onwheel = function () {
-    hihi.play();
-}
-li.onwheel = function () {
-    hihi.play();
-}
-
-huhu.onmouseup = function () {
-    hihi.play();
-}
-li.onmouseup = function () {
-    hihi.play();
-}
-
-control.onmouseover = function () {
+control.onclick = function () {
     control.style.background-color, rgb(182, 41, 41);
+
+}
+setTimeout(() => {
+  continues.classList.remove('active')
+},142000);
+
+control.onclick = function () {
+    message1.classList.remove('active')
+    message2.classList.remove('active')
+    clockdiv.classList.remove('active')
+    hihi.play();
+    control.classList.add('active')
+}
+continues.onclick = function () {
+    hihi.pause();
+    happyBirthday.classList.add('active')
+    videos.classList.remove('active')
 }
 
+
+//------------
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    return {
+      'total': t,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+  
+  function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+    function updateClock() {
+      var t = getTimeRemaining(endtime);
+  
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+  }
+  
+  var deadline = new Date(Date.parse(new Date()) + 1 * 1 * 2 * 61 * 1000);
+  initializeClock('clockdiv', deadline);
 
